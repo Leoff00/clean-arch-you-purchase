00602d77e5e1fc3448fa79e33699e57dbd60d3c6
leoff00 <leof0180@gmail.com>
2023-01-03 16:23:14 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-namespace": "off",
+    "no-unused-vars": "off",
+    "prefer-const": ["off", { allow: "let" }],
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      { allow: ["functions", "arrowFunctions"] },
+    ],
+    "no-use-before-define": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-function-return-type": [
+      "off",
+      { allowTypedFunctionExpressions: true },
+    ],
+  },
+};
