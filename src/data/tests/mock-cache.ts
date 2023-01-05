d97968fbac995b5b0dc18e5282fa5c2c56aaa34d
leoff00 <leof0180@gmail.com>
2023-01-05 17:45:12 -0300

    refactor: move CacheStoreSpy to its own file

---
+++ src/data/tests/mock-cache.ts
@@ -0,0 +1,34 @@
+import { CacheStore } from "@/data/protocols/cache";
+import { SavePurchases } from "@/domain/usecases";
+import { vi } from "vitest";
+
+export class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+  insertCallsCount = 0;
+  deleteKey: string;
+  insertKey: string;
+  insertValues: Array<SavePurchases.Params> = [];
+
+  delete(key: string): void {
+    this.deleteCallsCount++;
+    this.deleteKey = key;
+  }
+
+  insert(key: string, value: any): void {
+    this.insertCallsCount++;
+    this.insertKey = key;
+    this.insertValues = value;
+  }
+
+  simulateDeleteError(): void {
+    vi.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
+      throw new Error();
+    });
+  }
+
+  simulateInsertError(): void {
+    vi.spyOn(CacheStoreSpy.prototype, "insert").mockImplementationOnce(() => {
+      throw new Error();
+    });
+  }
+}
--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,39 +1,6 @@
-import { describe, expect, test, vi } from "vitest";
-import { CacheStore } from "@/data/protocols/cache";
+import { describe, expect, test } from "vitest";
 import { LocalSavePurchases } from "@/data/usecases/save-purchases";
-import { SavePurchases } from "@/domain/usecases";
-import { mockPurchases } from "@/data/tests";
-
-class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0;
-  insertCallsCount = 0;
-  deleteKey: string;
-  insertKey: string;
-  insertValues: Array<SavePurchases.Params> = [];
-
-  delete(key: string): void {
-    this.deleteCallsCount++;
-    this.deleteKey = key;
-  }
-
-  insert(key: string, value: any): void {
-    this.insertCallsCount++;
-    this.insertKey = key;
-    this.insertValues = value;
-  }
-
-  simulateDeleteError(): void {
-    vi.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
-      throw new Error();
-    });
-  }
-
-  simulateInsertError(): void {
-    vi.spyOn(CacheStoreSpy.prototype, "insert").mockImplementationOnce(() => {
-      throw new Error();
-    });
-  }
-}
+import { mockPurchases, CacheStoreSpy } from "@/data/tests";
 
 type SutTypes = {
   sut: LocalSavePurchases;
